3b32bb55826f54dbc420f1077cd45acfa96168f0
initialencounter <2911583893@qq.com>
2024-01-14 23:59:32 +0800

    fix(cuda12.1): entrypoint not work

--- cuda12.1/entrypoint/entrypoint.sh
+++ cuda12.1/entrypoint/entrypoint.sh
@@ -2,10 +2,22 @@
 # network config
 export PATH=/opt/miniconda3/bin/:$PATH
 export USER="root"
-conda deactivate
+. /opt/miniconda3/bin/deactivate
+
+if [ ! -d "/root/.jupyter" ]; then
+        mkdir -p "/root/.jupyter"
+fi
+
+if [ ! -d "/root/.vnc" ]; then
+        mkdir -p "/root/.vnc"
+fi
+
+if [ ! -f "/dev/tty0" ]; then
+        touch "/dev/tty0"
+fi
 
 if [ "$SSH_PORT" ]; then
-    cp -f /etc/ssh/sshd_config.bak /etc/ssh/sshd_config
+    cp -f /entrypoint/sshd_config.bak /etc/ssh/sshd_config
     sed -i "s/SSH_PORT/$SSH_PORT/" /etc/ssh/sshd_config
 fi
 
@@ -14,20 +26,20 @@ service ssh start
 printf "$ALL_PASSWD\n$ALL_PASSWD\n\n" | passwd
 
 #vnc password config
-cp -f /root/set_vnc_passwd.sh.bak /root/set_vnc_passwd.sh
+cp -f /entrypoint/set_vnc_passwd.sh.bak /root/set_vnc_passwd.sh
 sed -i "s/VNC_PASSWD/$ALL_PASSWD/" /root/set_vnc_passwd.sh
 expect /root/set_vnc_passwd.sh
 # vnc 
 if [ "$VNC_PORT" ]; then
     vnc_port=`expr $VNC_PORT - 5900`
-    cp -f /root/reset_vnc.sh.bak /root/reset_vnc.sh
+    cp -f /entrypoint/reset_vnc.sh.bak /root/reset_vnc.sh
     sed -i "s/VNC_PORT/$vnc_port/" /root/reset_vnc.sh
 fi
 
 if [ "$JUPYTER_PORT" ]; then
-    cp -f /root/.jupyter/jupyter_notebook_config.py.bak /root/.jupyter/jupyter_notebook_config.py
+    cp -f /entrypoint/jupyter_notebook_config.py.bak /root/.jupyter/jupyter_notebook_config.py
     sed -i "s/8888/$JUPYTER_PORT/" /root/.jupyter/jupyter_notebook_config.py
-    cp -f /usr/local/bin/start_vnc_jupyter.sh.bak /usr/local/bin/start_vnc_jupyter.sh
+    cp -f /entrypoint/start_vnc_jupyter.sh.bak /usr/local/bin/start_vnc_jupyter.sh
     sed -i "s/ALL_PASSWD/$ALL_PASSWD/" /usr/local/bin/start_vnc_jupyter.sh
     expect /usr/local/bin/start_vnc_jupyter.sh
 fi
