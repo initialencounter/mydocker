558ffd27b32e8a1532a4a3f467d33dc412addd0a
initialencounter <2911583893@qq.com>
2024-01-14 22:56:16 +0800

    chore(cuda12.1)

--- cuda12.1/entrypoint/set.sh
+++ cuda12.1/entrypoint/set.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt install expect -y
 
 if [ ! -d "/root/.jupyter" ]; then
         mkdir -p "/root/.jupyter"
@@ -17,3 +16,4 @@ cp -fp reset_vnc.sh.bak /root/reset_vnc.sh.bak
 cp -fp jupyter_notebook_config.py.bak /root/.jupyter/jupyter_notebook_config.py.bak
 cp -fp start_vnc_jupyter.sh.bak /usr/local/bin/start_vnc_jupyter.sh.bak
 cp -fp set_vnc_passwd.sh.bak /root/set_vnc_passwd.sh.bak
+cp -f pycharm/pycharm.desktop /usr/share/applications/pycharm.desktop
