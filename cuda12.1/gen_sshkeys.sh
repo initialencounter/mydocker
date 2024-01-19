ef821e0e19ee43279c171f0eed73148b2f2c4664
initialencounter <2911583893@qq.com>
2024-01-20 02:32:04 +0800

    fix(cuda12.1): CentOS7 ssh-keygen

---
+++ cuda12.1/gen_sshkeys.sh
@@ -0,0 +1,4 @@
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
+ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
+ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
