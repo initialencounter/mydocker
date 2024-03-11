b8eaed554ccfe4334782aed8c1f56866d9e8ff04
initialencounter <2911583893@qq.com>
2024-03-12 00:37:46 +0800

    fix: use volume failed

--- LiteLoader/base/start.sh
+++ LiteLoader/base/start.sh
@@ -3,8 +3,7 @@
 # 安装 Liteloader
 if [ ! -f "/LiteLoader/package.json" ]; then
     mkdir -p /LiteLoader && \
-    unzip /tmp/LiteLoaderQQNT.zip -d /LiteLoader && \
-    sed -i '1s/^/require("\/LiteLoader");\n/' /opt/QQ/resources/app/app_launcher/index.js
+    unzip /tmp/LiteLoaderQQNT.zip -d /LiteLoader
 fi
 
 service dbus start
