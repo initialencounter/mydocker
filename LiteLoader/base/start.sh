edb7dd59a5dae2850e3db8ded49e1cc838c2db15
风宝宝 <109729945+initialencounter@users.noreply.github.com>
2024-03-12 13:02:37 +0800

    fix(liteloader): dbus

--- LiteLoader/base/start.sh
+++ LiteLoader/base/start.sh
@@ -6,10 +6,21 @@ if [ ! -f "/LiteLoader/package.json" ]; then
     unzip /tmp/LiteLoaderQQNT.zip -d /LiteLoader
 fi
 
-service dbus start
-rm -f /tmp/.X1-lock
+echo "nameserver 114.114.114.114\nsearch lan" | tee /etc/resolv.conf > /dev/null
+mkdir /run/user/$(id -u)
 export DISPLAY=:1
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
+chmod 700 $XDG_RUNTIME_DIR
+chown $(id -un):$(id -gn) $XDG_RUNTIME_DIR
+
+rm -f /tmp/.X1-lock
+service dbus start
+sleep 2
 Xvfb :1 -screen 0 1280x1024x16 &
+sleep 2
+dbus-daemon --session --address=$DBUS_SESSION_BUS_ADDRESS --nofork --nopidfile --syslog-only &
+sleep 2
 fluxbox &
 sleep 2
 x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
