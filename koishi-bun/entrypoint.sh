ae6b03dd25c9f7fd43e2c7c4ddd7b51592e2574e
initialencounter <2911583893@qq.com>
2024-05-05 21:38:32 +0800

    fix(koishi-bun): path

--- koishi-bun/entrypoint.sh
+++ koishi-bun/entrypoint.sh
@@ -3,7 +3,8 @@ set -eu
 
 chown -R root:root /koishi
 if [ ! -e "/koishi/package.json" ]; then
-  unzip -d / /koishi.zip
+  unzip -d /tmp /koishi.zip
+  mv /tmp/koishi/* /koishi
   sed -Ei 's/(([[:space:]]*)maxPort.*)/\1\n\2host: 0.0.0.0/' /koishi/koishi.yml
 fi
 
