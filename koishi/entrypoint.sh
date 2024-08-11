167d03f4e8351edf53b8befe2e60d76a6096f050
initialencounter <2911583893@qq.com>
2024-08-12 01:00:41 +0800

    fix(koishi) yarn

--- koishi/entrypoint.sh
+++ koishi/entrypoint.sh
@@ -5,6 +5,7 @@ chown -R root:root /koishi
 if [ ! -e "/koishi/package.json" ]; then
   unzip -d /koishi /boilerplate.zip
   sed -Ei 's/(([[:space:]]*)maxPort.*)/\1\n\2host: 0.0.0.0/' /koishi/koishi.yml
+  sed -i.bak '/"packageManager":/d' /koishi/package.json
 fi
 
 exec "$@"
