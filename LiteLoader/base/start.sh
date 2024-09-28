43279d231b3787a774994fc9100df0e991593e7f
initialencounter <2911583893@qq.com>
2024-09-28 14:51:52 +0800

    feat(liteloader): whale
    Signed-off-by: initialencounter <2911583893@qq.com>

--- LiteLoader/base/start.sh
+++ LiteLoader/base/start.sh
@@ -6,6 +6,12 @@ if [ ! -f "/LiteLoader/package.json" ]; then
     unzip /tmp/LiteLoaderQQNT.zip -d /LiteLoader
 fi
 
+# 安装 Liteloader
+if [ ! -f "/LiteLoader/plugins/whale/manifest.json" ]; then
+    mkdir -p /LiteLoader/plugins/whale && \
+    unzip /tmp/whale.zip -d /LiteLoader/plugins/whale
+fi
+
 echo "nameserver 114.114.114.114" > /etc/resolv.conf
 echo "nameserver 223.5.5.5" >> /etc/resolv.conf
 mkdir /run/user/$(id -u)
@@ -16,6 +22,7 @@ chmod 700 $XDG_RUNTIME_DIR
 chown $(id -un):$(id -gn) $XDG_RUNTIME_DIR
 
 rm -f /tmp/.X1-lock
+mkdir -p ~/.vnc
 service dbus start
 sleep 2
 Xvfb :1 -screen 0 1080x760x16 &
@@ -24,10 +31,13 @@ dbus-daemon --session --address=$DBUS_SESSION_BUS_ADDRESS --nofork --nopidfile -
 sleep 2
 fluxbox &
 sleep 2
-x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
+if [ "$ENABLE_VNC" ]; then
+    x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
+    x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd
+fi
 sleep 2
-nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
-sleep 2
-x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd
+if [ "$ENABLE_NOVNC" ]; then
+    nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
+fi
 sleep 2
 qq --no-sandbox
