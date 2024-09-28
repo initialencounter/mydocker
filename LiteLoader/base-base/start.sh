4c65709ba7217b76c982e02130c0bc0c3f8aa784
initialencounter <2911583893@qq.com>
2024-09-28 13:18:38 +0800

    feat(liteloader:base): timezone entrypoint
    Signed-off-by: initialencounter <2911583893@qq.com>

---
+++ LiteLoader/base-base/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "nameserver 114.114.114.114" > /etc/resolv.conf
+echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+mkdir /run/user/$(id -u)
+export DISPLAY=:1
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
+chmod 700 $XDG_RUNTIME_DIR
+chown $(id -un):$(id -gn) $XDG_RUNTIME_DIR
+
+rm -f /tmp/.X1-lock
+mkdir -p ~/.vnc
+service dbus start
+sleep 2
+Xvfb :1 -screen 0 1080x760x16 &
+sleep 2
+dbus-daemon --session --address=$DBUS_SESSION_BUS_ADDRESS --nofork --nopidfile --syslog-only &
+sleep 2
+fluxbox &
+sleep 2
+if [ "$ENABLE_VNC" ]; then
+    x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
+    x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd
+fi
+sleep 2
+if [ "$ENABLE_NOVNC" ]; then
+    nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
+fi
+sleep 2
+/bin/bash
