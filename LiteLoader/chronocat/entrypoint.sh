8f3cb1d662d44fe59991225c0bf3c839dca2babf
initialencounter <2911583893@qq.com>
2024-10-08 20:23:46 +0800

    chore:(chronocat): 0.2.16 -> 0.2.19

--- LiteLoader/chronocat/entrypoint.sh
+++ LiteLoader/chronocat/entrypoint.sh
@@ -12,6 +12,8 @@ if [ ! -f "/LiteLoader/plugins/*Chronocat*/manifest.json" ]; then
     unzip /tmp/chronocat-llqqnt-engine-chronocat-api.zip  -d /LiteLoader/plugins/
     unzip /tmp/chronocat-llqqnt-engine-chronocat-event.zip  -d /LiteLoader/plugins/
     unzip /tmp/chronocat-llqqnt.zip -d /LiteLoader/plugins/
+    unzip /tmp/chronocat-llqqnt-engine-media.zip -d /LiteLoader/plugins/
+    unzip /tmp/chronocat-llqqnt-engine-crychiccat.zip -d /LiteLoader/plugins/
 fi
 
 bash /root/start.sh
